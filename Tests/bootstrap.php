32af913322d0d4d677a652a1bf5c960764f4adfd
Oswald De Riemaecker <oswald@continuousphp.com>
2015-05-16 01:42:21 +0200

    Fixing the Core Eden using vendor

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -7,7 +7,7 @@
  * distributed with this package.
  */
  
-require_once __DIR__.'/../../Core/Loader.php';
+require_once __DIR__.'/../vendor/eden/core/Eden/Core/Loader.php';
 Eden\Core\Loader::i()
 	->addRoot(true, 'Eden\\Core')
 	->addRoot(realpath(__DIR__.'/../..'), 'Eden\\Utility')
